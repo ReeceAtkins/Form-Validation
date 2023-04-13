a77057382df7aecc10f19e5bbddf9b584bed5ffd
Your Name <you@example.com>
2023-04-13 16:06:21 -0700

    Created typescipt file, started form validation

---
+++ ts/main.ts
@@ -0,0 +1,9 @@
+
+window.onload = function(){
+    let formBtn = <HTMLElement>document.querySelector("form > button"); // get button thats inside form
+    formBtn.onclick = main;
+}
+
+function main():void{
+     
+}
